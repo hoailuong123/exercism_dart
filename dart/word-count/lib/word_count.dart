dbe13057df828c486718b955b056c463cfb94582
HoaiLuong <hoailuong160202@gmail.com>
2023-07-18 16:07:11 +0700

    done word_count

--- dart/word-count/lib/word_count.dart
+++ dart/word-count/lib/word_count.dart
@@ -1,3 +1,22 @@
 class WordCount {
+  Map<String, int> countWords(String phrase) {
+    final words = phrase
+        .split(RegExp(r"[^\w']+"))
+        .where((word) => word.isNotEmpty)
+        .map((word) => word.toLowerCase());
 
+    final wordCount = <String, int>{};
+    for (final word in words) {
+      wordCount[word] = (wordCount[word] ?? 0) + 1;
+    }
+
+    return wordCount;
+  }
+}
+
+void main() {
+  final wordCount = WordCount();
+  final result =
+      wordCount.countWords('car: carpet as java: javascript!!&@\$%^&');
+  print(result);
 }
